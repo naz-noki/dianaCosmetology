be06f34399d8e5e66920049320c852676297ff2b
NoK1 <nazargolcheko@gmail.com>
2023-10-19 21:34:58 +0300

    Fix for footer.

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -3,8 +3,13 @@ const Feedback = () => {
     return (
         <section className='Feedback__wrapper' id='thirdChapter'>
             <h1 className="Feedback__title">
-                До/После
+                Пример лечения акне при помощи домашнего ухода
             </h1>
+            <div className="Feedback__text">
+                <p><b>Вот один из примеров консультации, впечатляет?</b></p>
+                <p>Поэтому, если ты столкнулась с проблемой выбора средств</p>
+                <p>для домашнего ухода – смело мне пиши!</p>
+            </div>
             <div className="Feedback__block">
                 <img src="/img/beforeAndAfter.jfif" className="Feedback__img" />
                 <img src="/img/sms.png" className="Feedback__sms" />
